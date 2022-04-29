31db80c0a40d8e2b942b4cc3d2dd4536608b46d8
Yash Jaiman <jaimanyash520@gmail.com>
2022-04-29 18:55:47 +0530

    Commit

---
+++ src/main/java/com/springtuts/springboot/SpringbootCrudWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.springtuts.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springtuts/springboot/SpringbootCrudWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springtuts.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
